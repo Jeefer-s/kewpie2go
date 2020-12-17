96a20f5fb47ff8bf67a1a1d489987c81d92c90d6
Jeffrey <ksng.ge@gmail.com>
2020-12-17 20:00:02 +0100

    .

---
+++ src/components/custom-button/custom-button.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import {} from './custom-button.styles'
+
+
+const CustomButton = () => (
+    
+)
---
+++ src/components/custom-button/custom-button.styles.jsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.div`
+  border: 1px black solid;
+`;
